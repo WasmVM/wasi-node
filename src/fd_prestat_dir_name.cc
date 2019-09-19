c42ad4e2f630c72f5cb0c1ea60e9e36453e5c40a
LuisHsu <luishsuplus@gmail.com>
2019-09-19 20:05:11 +0800

    Implement fd_prestat_dir_name

--- src/fd_prestat_dir_name.cc
+++ src/fd_prestat_dir_name.cc
@@ -30,6 +30,8 @@ napi_value fd_prestat_dir_name(napi_env env, napi_callback_info cbinfo) {
     if(napi_create_int32(env, __WASI_EINVAL, &result) != napi_ok) return nullptr;
     return result;
   };
+  napi_value context;
+  if(napi_get_reference_value(env, contextRef, &context) != napi_ok) return nullptr;
 
   // Get args
   __wasi_fd_t fd;
@@ -52,13 +54,14 @@ napi_value fd_prestat_dir_name(napi_env env, napi_callback_info cbinfo) {
   }
 
   // Get path name by fd
+  char filePath[pathBuf_len + 1];
   #ifdef __linux__
     char procPath[PATH_MAX];
     if(sprintf(procPath, "/proc/self/fd/%d", fd) < 0){
       if(napi_create_int32(env, __WASI_EINVAL, &result) != napi_ok) return nullptr;
       return result;
     }
-    if((resInt = call_readlink(procPath, pathBuf, pathBuf_len, nullptr)) != __WASI_ESUCCESS){
+    if((resInt = call_readlink(procPath, filePath, pathBuf_len, nullptr)) != __WASI_ESUCCESS){
       if(napi_create_int32(env, resInt, &result) != napi_ok) return nullptr;
       return result;
     }
@@ -72,6 +75,27 @@ napi_value fd_prestat_dir_name(napi_env env, napi_callback_info cbinfo) {
   #error "Can't get directory path in this operating system"
   #endif
   #endif
+  filePath[pathBuf_len] = '\0';
+
+  // Check capable
+  napi_value capablePath;
+  if(napi_get_named_property(env, context, "capable_path", &capablePath) != napi_ok) return nullptr;
+  uint32_t capableLen;
+  if(napi_get_array_length(env, capablePath, &capableLen) != napi_ok) return nullptr;
+  for(uint32_t i = 0; i < capableLen; ++i){
+    napi_value pathObj;
+    if(napi_get_element(env, capablePath, i, &pathObj) != napi_ok) return nullptr;
+    char pathStr[PATH_MAX];
+    if(napi_get_value_string_utf8(env, pathObj, pathStr, PATH_MAX, nullptr) != napi_ok) return nullptr;
+    if(!strcmp(filePath, pathStr)){
+      memset(filePath, '\0',pathBuf_len);
+      if(napi_create_int32(env, resInt, &result) != napi_ok) return nullptr;
+      return result;
+    }
+  }
+
+  // Set path if not capable
+  memcpy(pathBuf, filePath, pathBuf_len + 1);
 
   // Result
   if(napi_create_int32(env, resInt, &result) != napi_ok) return nullptr;
